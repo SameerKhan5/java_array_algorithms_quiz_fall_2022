28f14cebfdc685f1af3f57c6ddfb381bf69d0517
SameerKhan5 <87094020+SameerKhan5@users.noreply.github.com>
2023-01-11 08:41:28 -0500

    Create period_1_Khan_Sameer_ClimingClubA.java

---
+++ period_1_Khan_Sameer_ClimingClubA.java
@@ -0,0 +1,8 @@
+// Sameer Khan
+// Climbing Club A
+
+public void addClimb(String peakName, int climbTime)
+{
+    ClimbInfo newClimb = new ClimbInfo(peakName, climbTime);
+    climbList.add(newClimb);
+}
